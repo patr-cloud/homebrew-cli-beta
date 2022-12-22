619ea99add82506fd96c69386782fb27b17e84a7
Rakshith Ravi <rakshith@vicara.co>
2022-12-22 19:04:28 +0000

    patr cli updated to version 0.3.1

--- Formula/patr.rb
+++ Formula/patr.rb
@@ -1,8 +1,8 @@
 class Patr < Formula
 	desc "CLI for interacting with Patr resources"
 	homepage "https://github.com/patr-cloud/cli"
-	url "https://github.com/patr-cloud/cli/releases/download/v0.3.0/patr-0.3.0-darwin-amd64.tar.gz"
-	sha256 "35768cf05ca35e118ac89959ca02d3add979785eadd46a4f556f0a359e976ff5"
+	url "https://github.com/patr-cloud/cli/releases/download/v0.3.1/patr-0.3.1-darwin-amd64.tar.gz"
+	sha256 "54533b0ddd211dafe6b54ec46afca75b1263136289b4c75b75f4142a8b151957"
 
 	def install
 		bin.install 'patr'
